52a5f1ad710e57510f1437bdcba225cbf6affe61
Dawid Zakrzewski <deivxd@gmail.com>
2023-06-05 18:10:47 +0000

    4, 5, 6 challanges solution

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,18 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT FirstName, ReservationID, DATE 
+FROM Customers
+JOIN Reservations USING(CustomerID)
+WHERE FirstName = 'Norby'
+AND Date > '2022-07-24';
+
+DELETE FROM Reservations
+WHERE ReservationID = 2000;
+
+SELECT FirstName, ReservationID, DATE 
+FROM Customers
+JOIN Reservations USING(CustomerID)
+WHERE FirstName = 'Norby'
+AND Date > '2022-07-24';
